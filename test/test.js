58284542261e2ba5efc6237cfe4d4b4a40ae4c6e
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2015-05-26 01:24:49 +0530

    Custom message when max connections are reached - updated: source code, test & docs

--- test/test.js
+++ test/test.js
@@ -4,14 +4,14 @@ var express = require('express');
 var request = require('supertest');
 var rateLimit = require('../lib/express-rate-limit.js');
 
-describe('express-rate-limit node module', function () {
+describe('express-rate-limit node module', function() {
 
-    var start, delay;
+    var start, delay, message;
     var app;
 
     beforeEach(function() {
         start = Date.now();
-
+        message = 'You have been very naughty.. No API response for you!!';
     });
 
     afterEach(function() {
@@ -59,10 +59,26 @@ describe('express-rate-limit node module', function () {
             });
     }
 
+    function badRequestWithMessage(errorHandler, successHandler) {
+        request(app)
+            .get('/')
+            .expect(429)
+            .expect(message)
+            .end(function(err, res) {
+                if (err) {
+                    return errorHandler(err);
+                }
+                delay = Date.now() - start;
+                if (successHandler) {
+                    successHandler(null, res);
+                }
+            });
+    }
+
 
     it("should allow the first request with minimal delay", function(done) {
         createAppWith(rateLimit());
-        goodRequest(done, function(/* err, res */) {
+        goodRequest(done, function( /* err, res */ ) {
             delay = Date.now() - start;
             if (delay > 50) {
                 done(new Error("First request took too long: " + delay + "ms"));
@@ -76,12 +92,12 @@ describe('express-rate-limit node module', function () {
         createAppWith(rateLimit({
             delayMs: 100
         }));
-        goodRequest(done, function(/* err, res */) {
+        goodRequest(done, function( /* err, res */ ) {
             if (delay > 50) {
                 done(new Error("First request took too long: " + delay + "ms"));
             }
         });
-        goodRequest(done, function(/* err, res */) {
+        goodRequest(done, function( /* err, res */ ) {
             if (delay < 100) {
                 return done(new Error("Second request was served too fast: " + delay + "ms"));
             }
@@ -99,7 +115,7 @@ describe('express-rate-limit node module', function () {
         goodRequest(done);
         goodRequest(done);
         goodRequest(done);
-        goodRequest(done, function(/* err, res */) {
+        goodRequest(done, function( /* err, res */ ) {
             // should be about 300ms delay on 4th request - because the multiplier starts at 0
             if (delay < 300) {
                 return done(new Error("Fourth request was served too fast: " + delay + "ms"));
@@ -121,6 +137,17 @@ describe('express-rate-limit node module', function () {
         badRequest(done, done);
     });
 
+    it("should show the provided message instead of the default message when max connections are reached", function(done) {
+        createAppWith(rateLimit({
+            delayMs: 0,
+            max: 2,
+            message: message
+        }));
+        goodRequest(done);
+        goodRequest(done);
+        badRequestWithMessage(done, done);
+    });
+
     it("should (eventually) accept new connections from a blocked IP", function(done) {
         createAppWith(rateLimit({
             delayMs: 100,
@@ -132,7 +159,7 @@ describe('express-rate-limit node module', function () {
         badRequest(done);
         setTimeout(function() {
             start = Date.now();
-            goodRequest(done, function(/* err, res */) {
+            goodRequest(done, function( /* err, res */ ) {
                 if (delay > 50) {
                     done(new Error("Eventual request took too long: " + delay + "ms"));
                 } else {
