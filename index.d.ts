79c7637a52364d202ea47ddf13759ea76d2bc883
derSoerrn95 <soeren@borgstedt.net>
2019-05-29 19:03:05 +0200

    Update index.d.ts
    fixed ts 3.5.x error (only testet with ts 3.5.1)

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,39 @@
-import { RequestHandler, Request, Response, NextFunction } from 'express';
+import {RequestHandler, Request, Response, NextFunction} from 'express';
 
 export interface Options {
-    max?: number;
-    message?: any;
-    headers?: boolean;
-    windowMs?: number;
-    store?: Store | any;
-    statusCode?: number;
-    skipFailedRequests?: boolean;
-    skipSuccessfulRequests?: boolean;
-    skip?(req?: Request, res?: Response): boolean;
-    onLimitReached?(req?: Request, res?: Response): void;
-    handler?(req: Request, res: Response, next?: NextFunction): void;
-    keyGenerator?(req: Request, res?: Response): string | Request['ip'];
+  max?: number;
+  message?: any;
+  headers?: boolean;
+  windowMs?: number;
+  store?: Store | any;
+  statusCode?: number;
+  skipFailedRequests?: boolean;
+  skipSuccessfulRequests?: boolean;
+
+  skip?(req?: Request, res?: Response): boolean;
+
+  onLimitReached?(req?: Request, res?: Response): void;
+
+  handler?(req: Request, res: Response, next?: NextFunction): void;
+
+  keyGenerator?(req: Request, res?: Response): string | Request['ip'];
 }
 
 export interface Store {
-    hits: {
-        [key: string]: number;
-    };
-    resetAll(): void;
-    resetTime: number;
-    setInterval: NodeJS.Timeout;
-    resetKey(key: string | any): void;
-    decrement(key: string | any): void;
-    incr(key: string | any, cb: (err?: Error, hits?: number) => void): void;
+  hits: {
+    [key: string]: number;
+  };
+
+  resetAll(): void;
+
+  resetTime: number;
+  setInterval: NodeJS.Timeout;
+
+  resetKey(key: string | any): void;
+
+  decrement(key: string | any): void;
+
+  incr(key: string | any, cb: (err?: Error, hits?: number) => void): void;
 }
 
-declare function RateLimit(options?: Options): (req: Request, res: Response, next: NextFunction) => void;
-
-export = RateLimit;
+export declare function RateLimit(options?: Options): (req: Request, res: Response, next: NextFunction) => void;
