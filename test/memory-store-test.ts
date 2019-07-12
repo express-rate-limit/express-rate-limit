0ca96669ae188a5bd7e5bc97a712ba588914b423
Nathan Friedly <nathan@nfriedly.com>
2019-07-12 11:40:09 -0400

    convert (most of) memory store test to typescript

--- test/memory-store-test.ts
+++ test/memory-store-test.ts
@@ -1,13 +1,11 @@
-"use strict";
-
-const MemoryStore = require("../dist/memory-store.js");
+import MemoryStore = require("../dist/memory-store.js");
 
 describe("MemoryStore store", function() {
   it("sets the value to 1 on first incr", function(done) {
     const store = new MemoryStore(-1);
     const key = "test-store";
 
-    store.incr(key, function(err, value) {
+    store.incr(key, function(err?: Error, value?: Number) {
       if (err) {
         done(err);
       } else {
@@ -129,44 +127,6 @@ describe("MemoryStore store", function() {
           });
         }, 60);
       });
-    });
-  });
-
-  describe("timeout", function() {
-    const originalSetInterval = setInterval;
-    let timeoutId = 1;
-    let realTimeoutId;
-
-    beforeEach(function() {
-      timeoutId = 1;
-      // eslint-disable-next-line  no-global-assign
-      setInterval = function(callback, timeout) {
-        realTimeoutId = originalSetInterval(callback, timeout);
-        return timeoutId++;
-      };
-    });
-
-    it("can run in electron where setInterval does not return a Timeout object with an unset function", function(done) {
-      const store = new MemoryStore(-1);
-      const key = "test-store";
-
-      store.incr(key, function(err, value) {
-        if (err) {
-          done(err);
-        } else {
-          if (value === 1) {
-            done();
-          } else {
-            done(new Error("incr did not set the key on the store to 1"));
-          }
-        }
-      });
-    });
-
-    afterEach(function() {
-      // eslint-disable-next-line  no-global-assign
-      setInterval = originalSetInterval;
-      clearTimeout(realTimeoutId);
     });
   });
 
