a567f791ef36ef7aa5266573a00bb4d7f59b74d3
Vedant K <gamemaker0042@gmail.com>
2021-12-29 12:05:42 +0530

    test: use jest to run external store tests

--- test/external/stores/test/stores-test.ts
+++ test/external/stores/test/stores-test.ts
@@ -8,26 +8,14 @@ import mongoStore from '../source/mongo-store.js'
 import memcachedStore from '../source/memcached-store.js'
 import preciseStore from '../source/precise-store.js'
 
-for (const [name, app] of [
+test.each([
 	['redis', redisStore],
 	['mongo', mongoStore],
 	['memcached', memcachedStore],
 	['precise', preciseStore],
-]) {
-	console.log('should work for %s store', name)
-
-	try {
-		await request(app).get('/').expect(200)
-		await request(app).get('/').expect(200)
-		await request(app).get('/').expect(200)
-		await request(app).get('/').expect(429)
-	} catch (error) {
-		console.error('did not work for %s store', name)
-		console.error(error)
-		process.exit(1)
-	}
-
-	console.log('worked for %s store', name)
-}
-
-process.exit(0)
+])('should work for %s store', async (_name, app) => {
+	await request(app).get('/').expect(200)
+	await request(app).get('/').expect(200)
+	await request(app).get('/').expect(200)
+	await request(app).get('/').expect(429)
+})
