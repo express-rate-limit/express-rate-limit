63800e71bb4e2212aa18021d4aed1042bd510c2f
Vedant K <gamemaker0042@gmail.com>
2022-01-05 20:35:31 +0530

    fix: use named imports for express types

--- test/library/helpers/create-server.ts
+++ test/library/helpers/create-server.ts
@@ -1,38 +1,40 @@
 // /test/helpers/create-server.ts
 // Create an Express server for testing
 
-import Express from 'express'
+import createExpressApplication, {
+	Application,
+	Request,
+	Response,
+	RequestHandler,
+} from 'express'
 
 /**
  * Create an Express server with the given middleware
  *
- * @param middleware {Express.RequestHandler | Express.RequestHandler[]} - The middleware
+ * @param middleware {RequestHandler | RequestHandler[]} - The middleware
  *
  * @returns {Express.Application} - The express server
  */
 export const createServer = (
-	middleware: Express.RequestHandler | Express.RequestHandler[],
-): Express.Application => {
+	middleware: RequestHandler | RequestHandler[],
+): Application => {
 	// Create an Express server
-	const app = Express() // eslint-disable-line new-cap
+	const app = createExpressApplication()
 
 	// Use the middleware
 	app.use(middleware)
 
 	// Register test routes
-	app.all('/', (_request: Express.Request, response: Express.Response) =>
+	app.all('/', (_request: Request, response: Response) =>
 		response.send('Hi there!'),
 	)
-	app.get('/error', (_request: Express.Request, response: Express.Response) =>
+	app.get('/error', (_request: Request, response: Response) =>
 		response.sendStatus(400),
 	)
-	app.post(
-		'/crash',
-		(_request: Express.Request, response: Express.Response) => {
-			response.emit('error', new Error('Oops!'))
-			response.end()
-		},
-	)
+	app.post('/crash', (_request: Request, response: Response) => {
+		response.emit('error', new Error('Oops!'))
+		response.end()
+	})
 
 	// Return the application instance
 	return app
