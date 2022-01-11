a0b84749274e1734571db5f46880f2e13deac25a
Vedant K <gamemaker0042@gmail.com>
2022-01-11 08:24:22 +0530

    feat: add named export too

--- source/index.ts
+++ source/index.ts
@@ -3,7 +3,15 @@
 
 import rateLimit from './lib.js'
 
+// Export all the types as named exports
 export * from './types.js'
+
+// Export the rateLimit function as a default export
 // https://github.com/timocov/dts-bundle-generator/issues/182
 // eslint-disable-next-line unicorn/prefer-export-from
 export default rateLimit
+
+// Also export it as a named export if the default export does not work
+// (see https://github.com/nfriedly/express-rate-limit/issues/280)
+
+export { default as rateLimit } from './lib.js'
