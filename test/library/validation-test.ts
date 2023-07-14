0a26f1588b190b3edcfa43bfdb1bb528e280590e
Nathan Friedly <nathan@nfriedly.com>
2023-07-13 21:26:14 -0400

    additional validations tests

--- test/library/validation-test.ts
+++ test/library/validation-test.ts
@@ -12,42 +12,98 @@ import {
 import { Validations } from '../../source/validations.js'
 
 describe('validations tests', () => {
+	let validate = new Validations(true)
+
 	beforeEach(() => {
 		jest.spyOn(console, 'error').mockImplementation(() => {})
 	})
 	afterEach(() => {
 		jest.restoreAllMocks()
+		validate = new Validations(true)
 	})
 
 	// This one is a little more extensive because we're also testing the privat
 	// `runCheck` method.
-	describe('validateIp', () => {
-		const enabledValidator = new Validations(true)
-		const disabledValidator = new Validations(false)
-
-		it('should do nothing when validation is off', () => {
-			disabledValidator.ip('badip')
-			expect(console.error).not.toBeCalled()
-		})
-
+	describe('validate.ip', () => {
 		it('should allow a valid IPv4', () => {
-			enabledValidator.ip('1.2.3.4')
+			validate.ip('1.2.3.4')
 			expect(console.error).not.toBeCalled()
 		})
 
 		it('should allow a valid IPv6', () => {
-			enabledValidator.ip('1200:0000:AB00:1234:0000:2552:7777:1313')
+			validate.ip('1200:0000:AB00:1234:0000:2552:7777:1313')
 			expect(console.error).not.toBeCalled()
 		})
 
 		it('should log an error for an invalid IP', () => {
-			enabledValidator.ip('badip')
+			validate.ip('badip')
 			expect(console.error).toBeCalled()
 		})
 
 		it('shoud log an error for an undefined IP', () => {
-			enabledValidator.ip(undefined)
+			validate.ip(undefined)
 			expect(console.error).toBeCalled()
+		})
+
+		it('should log an error for an IPv4 with a port', () => {
+			validate.ip('1.2.3.4:1234')
+			expect(console.error).toBeCalled()
+		})
+
+		it('should log an error for an IPv6 with a port', () => {
+			validate.ip('[1200:0000:AB00:1234:0000:2552:7777:1313]:1234')
+			expect(console.error).toBeCalled()
+		})
+	})
+
+	describe('trustProxy', () => {
+		it('should log an error on "trust proxy" = true', () => {
+			validate.trustProxy({ app: { get: () => true } } as any)
+			expect(console.error).toBeCalled()
+		})
+		it('should not log an error on "trust proxy" != true', () => {
+			validate.trustProxy({ app: { get: () => false } } as any)
+			validate.trustProxy({ app: { get: () => '1.2.3.4' } } as any)
+			validate.trustProxy({ app: { get: () => /1.2.3.4/ } } as any)
+			validate.trustProxy({ app: { get: () => ['1.2.3.4'] } } as any)
+			expect(console.error).not.toBeCalled()
+		})
+	})
+
+	describe('xForwardedFor', () => {
+		it('should log an error only with X-Forwarded-For header and "trust proxy" = false', () => {
+			validate.xForwardedForHeader({
+				app: { get: () => true },
+				headers: { 'x-forwarded-for': '1.2.3.4' },
+			} as any)
+			validate.xForwardedForHeader({
+				app: { get: () => true },
+				headers: {},
+			} as any)
+			validate.xForwardedForHeader({
+				app: { get: () => false },
+				headers: {},
+			} as any)
+			expect(console.error).not.toBeCalled()
+			validate.xForwardedForHeader({
+				app: { get: () => false },
+				headers: { 'x-forwarded-for': '1.2.3.4' },
+			} as any)
+			expect(console.error).toBeCalled()
+		})
+	})
+
+	describe('disable', () => {
+		it('should initialize disabled when passed false', () => {
+			const disabledValidator = new Validations(false)
+			disabledValidator.ip('badip')
+			expect(console.error).not.toBeCalled()
+		})
+
+		it('should do nothing after disable() is called', () => {
+			validate.disable()
+			validate.ip('badip')
+			expect(console.error).not.toBeCalled()
 		})
 	})
 })
