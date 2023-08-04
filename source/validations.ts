1a25864cd6d43a28dcf7785482a0f9036468553c
Vedant K <gamemaker0042@gmail.com>
2023-08-04 16:46:29 +0530

    chore: use shorter link for error urls

--- source/validations.ts
+++ source/validations.ts
@@ -21,7 +21,7 @@ class ValidationError extends Error {
 	 * describing the issue in detail.
 	 */
 	constructor(code: string, message: string) {
-		const url = `https://github.com/express-rate-limit/express-rate-limit/wiki/Error-Codes#${code.toLowerCase()}`
+		const url = `https://express-rate-limit.github.io/errors/#${code.toLowerCase()}`
 		super(`${message} See ${url} for more information on this error.`)
 
 		// `this.constructor.name` is the class name
