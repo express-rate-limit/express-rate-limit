81c01d2fa64530a73db72d37fea4c287d1c2495b
Vedant K <gamemaker0042@gmail.com>
2025-06-24 23:03:57 +0530

    chore: fix formatting

--- test/external/imports/named-import/ts-cjs/eslint.config.mjs
+++ test/external/imports/named-import/ts-cjs/eslint.config.mjs
@@ -1,21 +1,21 @@
-import globals from 'globals'
-import { defineConfig } from 'eslint/config'
-import tseslint from 'typescript-eslint'
 import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended']
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    languageOptions: { globals: globals.node }
-  },
-  {
-    files: ['**/*.js'],
-    languageOptions: { sourceType: 'commonjs' }
-  },
-  tseslint.configs.recommended,
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+	},
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		languageOptions: { globals: globals.node },
+	},
+	{
+		files: ['**/*.js'],
+		languageOptions: { sourceType: 'commonjs' },
+	},
+	tseslint.configs.recommended,
 ])
