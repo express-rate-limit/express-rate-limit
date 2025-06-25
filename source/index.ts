3acabe4046bf7c8f64ccf7ce2ef7b4c20f8bf731
Vedant K <gamemaker0042@gmail.com>
2025-06-25 13:56:28 +0530

    fix: export memory store as a named export

--- source/index.ts
+++ source/index.ts
@@ -5,9 +5,11 @@
 export { ipKeyGenerator } from './ip-key-generator.js'
 // Export the memory store in case someone wants to use or extend it
 // (see https://github.com/nfriedly/express-rate-limit/issues/289)
-export { default as MemoryStore } from './memory-store.js'
+export { MemoryStore } from './memory-store.js'
+
 // Export the rateLimit function as a default export and as a named export, if
 // the default export does not work (see https://github.com/nfriedly/express-rate-limit/issues/280)
 export { default, default as rateLimit } from './rate-limit.js'
+
 // Export all the types as named exports
 export * from './types.js'
