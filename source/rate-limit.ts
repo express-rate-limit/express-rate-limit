cf38389b0423bffdba52e5ffdb865d7370c1ca4d
Vedant K <gamemaker0042@gmail.com>
2025-07-04 15:45:22 +0530

    fix: use `?.()` syntax to call func if not undefined

--- source/rate-limit.ts
+++ source/rate-limit.ts
@@ -306,7 +306,7 @@ const handleAsyncErrors =
 			await Promise.resolve(fn(request, response, next)).catch(next)
 		} catch (error: unknown) {
 			/* istanbul ignore next */
-			if (next) next(error)
+			next?.(error)
 		}
 	}
 
