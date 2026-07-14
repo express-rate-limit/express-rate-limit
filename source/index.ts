36695fbc48c29ef88cfd6477aed4d1865c4e18d1
Nathan Friedly <nathan@nfriedly.com>
2026-07-14 11:20:26 -0400

    Comments and spacing

--- source/index.ts
+++ source/index.ts
@@ -3,15 +3,17 @@
 
 // Export the IP address based key generator in case someone wants to use it.
 export { ipKeyGenerator } from './ip-key-generator.js'
+
 // Export the memory store in case someone wants to use or extend it
 // (see https://github.com/nfriedly/express-rate-limit/issues/289)
 export { MemoryStore } from './memory-store.js'
+
 // Export the rateLimit function as a default export and as a named export, if
 // the default export does not work (see https://github.com/nfriedly/express-rate-limit/issues/280)
-export {
-	default,
-	default as rateLimit,
-} from './rate-limit.js'
+export { default, default as rateLimit } from './rate-limit.js'
+
+// DAY, HOUR, MINUTE, & SECOND constants for more readable windowMS configurations
 export * from './time-constants.js'
+
 // Export all the types as named exports
 export * from './types.js'
