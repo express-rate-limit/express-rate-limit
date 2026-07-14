7b05e0dbd8b838075498af4fee7eccff4bd6c8a5
Nwankwo Kelechi Bomo <84792410+NwankwoKc@users.noreply.github.com>
2026-07-14 16:39:46 +0100

    feat: add time constants to support more readable values for windowMs (#655)
    * feat:Support more human readable approach to windowMs option
    * refc:change location of SECOND,MINUTE,HOUR constant to a new file
    
    ---------
    
    Co-authored-by: Nathan Friedly <nathan@nfriedly.com>

--- source/index.ts
+++ source/index.ts
@@ -3,6 +3,7 @@
 
 // Export the IP address based key generator in case someone wants to use it.
 export { ipKeyGenerator } from './ip-key-generator.js'
+
 // Export the memory store in case someone wants to use or extend it
 // (see https://github.com/nfriedly/express-rate-limit/issues/289)
 export { MemoryStore } from './memory-store.js'
@@ -10,6 +11,9 @@ export { MemoryStore } from './memory-store.js'
 // Export the rateLimit function as a default export and as a named export, if
 // the default export does not work (see https://github.com/nfriedly/express-rate-limit/issues/280)
 export { default, default as rateLimit } from './rate-limit.js'
+
+// DAY, HOUR, MINUTE, & SECOND constants for more readable windowMS configurations
+export * from './time-constants.js'
 
 // Export all the types as named exports
 export * from './types.js'
